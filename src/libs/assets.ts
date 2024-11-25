ec83390e2a1c132e99074bac810816f437e01a9b
₿igto <bigto@mantra.finance>
2024-11-25 14:45:55 +0800

    chore(codes): Improve error handling

--- src/libs/assets.ts
+++ src/libs/assets.ts
@@ -11,29 +11,35 @@ export function getDenomDetails(denom: string) {
 }
 
 export function findNonZeroExponent(denomUnits: AssetDenomUnit[]): AssetDenomUnit {
-  return <AssetDenomUnit>denomUnits.find((unit) => unit.exponent !== 0)
-    || { exponent: 0, base: '' };
+  const unit = denomUnits.find((unit) => unit.exponent !== 0);
+  return unit ?? { exponent: 0, denom: '' };
 }
 
 export async function formatIbcToken(
-  coin: { amount: string, denom: string },
+  input: { amount: string, denom: string },
   withIBCHash: boolean = true
 ) {
+  const coin = { ...input };
   const formatter = useFormatter();
   if (coin.denom.startsWith('ibc/')) {
-    const data = await formatter.fetchDenomTrace(coin.denom)
-    const assetDetail = getDenomDetails(data.base_denom)
+    try {
+      const data = await formatter.fetchDenomTrace(coin.denom)
+      const assetDetail = getDenomDetails(data.base_denom)
 
-    if (assetDetail && assetDetail.denom_units) {
-      const units = findNonZeroExponent(assetDetail.denom_units)
-      coin.denom = withIBCHash
-        ? `${coin.denom} (${units.denom.toUpperCase()})`
-        : units.denom.toUpperCase()
-      coin.amount = Decimal.fromAtomics(coin.amount, units.exponent).toString()
-    } else {
-      coin.denom = withIBCHash  ?
-        `${coin.denom} (${data.base_denom})`
-        : data.base_denom
+      if (assetDetail && assetDetail.denom_units) {
+        const units = findNonZeroExponent(assetDetail.denom_units)
+        coin.denom = withIBCHash
+          ? `${coin.denom} (${units.denom.toUpperCase()})`
+          : units.denom.toUpperCase()
+        coin.amount = Decimal.fromAtomics(coin.amount, units.exponent).toString()
+      } else {
+        coin.denom = withIBCHash  ?
+          `${coin.denom} (${data.base_denom})`
+          : data.base_denom
+      }
+    } catch (error) {
+      console.error('Failed to fetch denom trace:', error);
+      return coin; // Return original coin on error
     }
   }
   return coin;
